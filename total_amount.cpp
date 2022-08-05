5ee63ea03fd704eb1b5ed7c19e2917d4f5919862
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-08-05 12:59:09 +0500

    total_amount

---
+++ total_amount.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int amnt1,amnt2,sum;
+    cout<<"Enter the first amount : ";
+    cin>>amnt1;
+    cout<<"Enter the second amount : ";
+    cin>>amnt2;
+    sum=amnt1+amnt2;
+    cout<<"Total amount is "<<sum<<endl;
+    return 0;
+}
