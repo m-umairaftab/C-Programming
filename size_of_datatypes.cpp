2aed6cf1a9eab9d0bd791a2f38cdb23ed20e9dea
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-08-05 12:36:55 +0500

    size_of_datatypes

---
+++ size_of_datatypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace  std;
+int main(){
+    int a;
+    float b;
+    char c;
+    bool d;
+    long int e;
+    cout<<"Size of int is : "<<sizeof(a)<<endl;
+    cout<<"Size of float is : "<<sizeof(b)<<endl;
+    cout<<"Size of char is : "<<sizeof(c)<<endl;
+    cout<<"Size of bool is : "<<sizeof(d)<<endl;
+    cout<<"Size of long int is : "<<sizeof(e)<<endl;
+    return 0;
+}
