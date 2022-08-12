aea208420feb4a9afc7ec896e76e0e003b6b5d37
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-08-12 11:37:07 +0500

    how to use string

--- string.cpp
+++ string.cpp
@@ -1,14 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string str,str1,str2;
+    string str,str1,str2,str3,str4="786";
     cout<<"Enter the first string : ";
-    cin>>str1;
+    getline(cin,str1);
     cout<<"Enter the second string : ";
-    cin>>str2;
+    getline(cin,str2);
+    int add=stoi(str4);
+    cout<<add+2<<endl;
+    cout<<to_string(add)+"2"<<endl;
+    str3="my name is umair";
+    cout<<str3.find("name")<<endl;
+    cout<<str3<<endl;
+    sort(str3.begin(),str3.end());
+    cout<<str3<<endl;
+    cout<<str3.size()<<endl;
+    cout<<str3.insert(3,"name")<<endl;
+    cout<<str3.substr(3,4)<<endl;
+    str3.clear();
+    if(str3.empty()){
+        cout<<"String is empty."<<endl;
+    }
     str=str1+str2;
     cout<<str<<endl;
     str[3]='d';
     cout<<str<<endl;
+    str.erase(3,2);
+    cout<<str<<endl;
+    if(str1.compare(str2)){
+        cout<<"strings are equal."<<endl;
+    }
+    else{
+        cout<<"strings are not equal."<<endl;
+    }
     return 0;
 }
