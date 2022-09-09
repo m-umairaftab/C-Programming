8f942418b37accef41b5c3792abf84021eb60f47
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-09-09 12:28:50 +0500

    find cube of volume using function with one argument

---
+++ lab_journal/cube_with_one_argument.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void volume(int num){
+    cout<<"Volume of a cube is "<<num*num*num<<endl;
+}
+int main(){
+    int num;
+    cout<<"Enter the any side of volume : ";
+    cin>>num;
+    volume(num);
+    return 0;
+}
