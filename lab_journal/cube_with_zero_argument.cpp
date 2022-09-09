f4dd4f7865f73f42f4663a00e301476fe222c3c9
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-09-09 12:21:22 +0500

    find volume of a cube using function with zero argument

---
+++ lab_journal/cube_with_zero_argument.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+void volume(){
+    int num;
+    cout<<"Enter the any side of volume : ";
+    cin>>num;
+    cout<<"Volume of a cube is "<<num*num*num<<endl;
+}
+int main(){
+    volume();
+    return 0;
+}
