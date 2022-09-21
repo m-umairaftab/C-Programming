f42c412f5bbe51722be8337bfbb9c600f5c46efc
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-09-21 16:25:44 +0500

    Defult Argument

---
+++ functions/defult_argument.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void repchar(char='*', int=45);      //declaration with default arguments
+int main()
+{
+    repchar();                       //prints 45 asterisks
+    repchar('=');                    //prints 45 equal signs
+    repchar('+', 30);                //prints 30 plus signs
+    return 0;
+}
+void repchar(char ch, int n)        //defaults supplied
+{                                   //if necessary
+    for(int j=0; j<n; j++)          //loops n times
+        cout << ch;                 //prints ch
+    cout << endl;
+}
