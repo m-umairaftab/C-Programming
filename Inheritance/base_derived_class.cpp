f1df5380597a2ad305e91f75e832d4c1fb9d04c3
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-09-30 12:02:30 +0500

    Base and Derived class

---
+++ Inheritance/base_derived_class.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Counter                                       //base class
+{
+    protected:                                              //NOTE: not private
+        unsigned int count;                                 //count
+    public:
+        Counter() : count(0)                                //no-arg constructor
+        { }
+        Counter(int c) : count(c)                           //1-arg constructor
+        { }
+        unsigned int get_count() const                      //return count
+        { 
+            return count;                                   //incr count (prefix)
+        }
+        Counter operator ++ ()                             
+        { 
+            return Counter(++count);
+        }
+};
+class CountDn : public Counter                              //derived class
+{
+    public:
+        Counter operator -- ()                                      //decr count (prefix)
+        { 
+            return Counter(--count); 
+        }
+};
+int main(){
+    CountDn c1;                                                 //c1 of class CountDn
+    cout << "\nc1=" << c1.get_count();                          //display c1
+    ++c1; ++c1; ++c1;                                           //increment c1, 3 times
+    cout << "\nc1" << c1.get_count();                           //display it
+    --c1; --c1;                                                 //decrement c1, twice
+    cout << "\nc1" << c1.get_count();                           //display it
+    cout << endl;
+    return 0;
+}
