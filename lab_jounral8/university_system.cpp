36f7ff5ebbb4a886de44b3968ea4501ea51947e4
Umair Aftab <m.umair.umairaftab@gmail.com>
2022-10-05 11:52:01 +0500

    University Admission System

--- lab_jounral8/university_system.cpp
+++ lab_jounral8/university_system.cpp
@@ -1,10 +1,11 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 class Student{
+    protected:
+      string studentName, degree;
+      int dateOfBirth;
     public:
-        string studentName, degree;
-        int dateOfBirth;
-        float cgpa, marks;
         void studentInfo(){
             cout<<"Enter the name of studet : ";
             getline(cin, studentName);
@@ -13,46 +14,46 @@ class Student{
             cout<<"Enter the year in which born : ";
             cin>>dateOfBirth;
         }
-        void examinationSystem(){
+};
+class ExaminationSystem : public Student {
+    protected:
+        float cgpa, marks;
+    public:
+        void examSystem(){
             int commingFrom;
             cout<<"Enter your examination system (1 for annual and 0 for semester): ";
             cin>>commingFrom;
             if(commingFrom==1){
                 cout<<"Enter the percentage marks that are obtained in annual exam : ";
                 cin>>marks;
-               isEligibleMarks(marks, dateOfBirth);
+                if(marks >= 60 & dateOfBirth >= 1980){
+                    cout<<"Student is eligible for admission.\n";
+                }
+                else{
+                    cout<<"Student is not eligible for admission.\n";
+                }
             }
             else if(commingFrom==0){
                 cout<<"Enter the cumulative grade point average (CGPA) : ";
                 cin>>cgpa;
-                isEligibleCgpa(cgpa);
+                if(cgpa >= 3.0){
+                    cout<<"Student is eligible for admission.\n";
+                }
+                else{
+                    cout<<"Student is not eligible for admission.\n";
+                }
             }
             else{
                 cout<<"Enter the correct input.\n";
-                examinationSystem();
-            }
-        }
-        void isEligibleMarks(float marks, int dateOfBirth){
-            if(marks >= 60 & dateOfBirth >= 1980){
-                cout<<"Student is eligible for admission.\n";
-            }
-            else{
-                cout<<"Student is not eligible for admission.\n";
-            }
-        }
-        void isEligibleCgpa(float cgpa){
-            if(cgpa >= 3.0){
-                cout<<"Student is eligible for admission.\n";
-            }
-            else{
-                cout<<"Student is not eligible for admission.\n";
+                examSystem();
             }
         }
 };
 int main(){
-    Student detail;
-    detail.studentInfo();
-    detail.examinationSystem();
-    //detail.isEligible();
+    ExaminationSystem system;
+    
+    system.studentInfo();
+   
+    system.examSystem();
     return 0;
 }
