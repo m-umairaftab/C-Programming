cae9bac8ae22e85cccb8d3df37c1de6971e01690
m-umairaftab <m.umair.umairaftab@gmail.com>
2023-09-27 16:08:37 +0500

    Guess Secreat Number

--- guess_secret_number.cpp
+++ guess_secret_number.cpp
@@ -22,23 +22,32 @@ void playerTurn(){
     else if(choice==scrtNum & turn=='X'){
         cout<<p2<<" wins!Congratulation\n";
     }
-    else if(draw == 20){
-        cout<<"Computer wins!\n";
+    else if(draw == 10){
+        cout<<"Both player can't find screat number.So,Computer wins!\n";
     }
     else{
         playerTurn();
     }
 }
 
-int main(){
-    srand(time(0));
+void PlayerName(string pl1, string pl2){
     cout<<"Enter the name of player1 : ";
 	getline(cin,p1);
 	cout<<"Enter the name of player2 : ";
 	getline(cin,p2);
-	cout<<p1<<" is player1 so he/she will play first.\n";
+    cout<<p1<<" is player1 so he/she will play first.\n";
 	cout<<p2<<" is player2 so he/she will play second.\n";
+}
+
+void generateSecreatNum(int scrtNum){
+    srand(time(0));
     scrtNum=rand()%10;
+}
+
+int main(){
+    
+    PlayerName(p1, p2);
+	generateSecreatNum(scrtNum);
     playerTurn();
     return 0;
 }
